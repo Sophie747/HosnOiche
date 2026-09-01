fdd5b9acb6154839ddac4fd763d6d65e7a9aee8a
strele <test@example.com>
2026-09-01 16:05:17 +0300

    fix: footer link

--- frontend/src/components/AppFooter.vue
+++ frontend/src/components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
   <footer class="bg-gray-200 text-center p-4 text-gray-500 text-sm mt-auto w-full">
-    <a href="www.lstronix.at">Hosted By LStronix.at</a>
+    <a target="_blank" rel="noopener noreferrer" href="https://lstronix.at/">Hosted By LStronix.at</a>
   </footer>
 </template>
